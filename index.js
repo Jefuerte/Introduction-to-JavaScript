3dd1ba93c0bdda0df713613bac6b42652968ee41
jessica <fuerte.jessica10@gmail.com>
2021-11-02 17:54:57 -0500

    did annoyingSong func

--- index.js
+++ index.js
@@ -233,10 +233,13 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(sodaNum){
+      for (let i = sodaNum; i >= 0; i--){
+        console.log(`${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around 
+        ${i - 1} bottles of soda on the wall`)
+      }
 }
-
+console.log(annoyingSong(10))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
